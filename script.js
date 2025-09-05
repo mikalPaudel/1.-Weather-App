394c37c443da6bdf42b06ce607045509cdc21d26
Mikal <mikalpaudel58@gmail.com>
2025-09-05 19:32:33 +0545

    Implement weather icon updates and display logic in the weather application

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 const apiKey = "a4227f8a20e9e35a2c7a8f0e4cd70386"; // Replace with your OpenWeatherMap API key
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=kathmandu";
-
-const async function checkWeather() {
-    const response = await fetch(apiUrl + "&appid=${apiKey}");
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
-  
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+    // document.querySelector(".description").innerHTML = data.weather[0].description;
+    // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + data.name + "')";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  }
+    else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+    }
+    else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "images/snow.png";
+    }
+    else if (data.weather[0].main == "Thunderstorm") {
+    weatherIcon.src = "images/thunderstorm.png";
+    }
+    else {
+    weatherIcon.src = "images/weather.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
 }
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+checkWeather(city);
